5c11aeb76423b3dc390cf5d332cc514ab5c212f6
lupipedraza <42683132+lupipedraza@users.noreply.github.com>
2019-04-03 18:57:09 -0300

    Add files via upload

--- Percolar.c
+++ Percolar.c
@@ -4,53 +4,80 @@
 #include <stdlib.h>
 #include <time.h>
 
+//---------------------AVISO LAS FUNCIONES QUE VAMOS A USAR
 int poblar(int *red,float p,int dim);
-int imprimir(int *red, int dim);
-int clasificar(int *red, int dim);
-int actualizar(int *local,int *historial,int s,int frag);
-int etiqueta_falsa(int *local, int *historial, int s1, int s2, int frac);
+int imprimirMat(int *red, int dim);
+int imprimirVector(int *vect, int dim);
+int clasificar(int *red, int dim, int *historial,int etiqueta);
+int actualizar(int *local,int *historial,int s,int etiqueta);
+int etiqueta_falsa(int *local, int *historial, int s1, int s2, int etiqueta);
+int arreglar_etiquetas(	int *red,int *historial, int dim);
+int percola(int *red, int dim);
 
+//---------------------Main--------------------------------
 int main()
 
 {
+
+	srand(time(NULL));
 	int dim = 5;
     	float p = 0.5;
+	int etiqueta=2;
 	int *red;
+	int b;
 	red=(int *)malloc(dim*dim*sizeof(int));  
+	int *historial;
+	historial=(int *)malloc(dim*dim*sizeof(int));
+	int r;
+	for(r=0;r<dim*dim;r++)//Armar el historial
+	{*(historial+r)=r;
+	}
+
 	poblar(red, p,dim);
-	imprimir(red,dim);
-	clasificar(red,dim);
-	printf("\n");
-	imprimir(red,dim);
-	free(red);	
-	printf("\n");
+	printf("Red Originial: \n");	
+	imprimirMat(red,dim);
+
+	clasificar(red,dim,historial,etiqueta);
+	printf("Historial: \n");	
+	imprimirVector(historial,dim);
 	
+	printf("Red modificada: \n");	
+	imprimirMat(red,dim);
+
+	arreglar_etiquetas(red, historial, dim);
+	printf("Red arreglada: \n");	
+	imprimirMat(red,dim);
+
+	b=percola(red, dim);
+	if (b)
+	{printf("¡Percoló!");
+	}else{
+	printf("Nope, no percoló"); 	
+	}
+	
+	free(red);		
 	return 0;
 }
 
+//---------------------FUNCIONES--------------------------------
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 int poblar(int *red,float p,int dim)
 {
-
-    int i;  
-    float rndm;
-    srand(time(NULL));    
-    for (i=0; i<dim*dim; i++)
+  int i;
+  float rndm;
+  for (i=0; i<dim*dim; i++)
+  {
+    rndm=(float)rand()/(float)RAND_MAX;
+    *(red+i)=0;
+    if (rndm < p)
     {
-        rndm=(float)rand()/(float)RAND_MAX;
-
-     	//  printf("%d", rndm);
-      	//  printf("\n");
-        *(red+i)=0;
-        if (rndm < p)
-        {
-            *(red+i)=1;
-        }
-
-    } 
-    return 0;
+        *(red+i)=1;
+    }
+  }
+  return 0;
 }
-
-int imprimir(int *red, int dim)
+//*************************************************************
+int imprimirMat(int *red, int dim)
     {
         int k, l;
         for (k=0; k<dim; k++)
@@ -61,21 +88,28 @@ int imprimir(int *red, int dim)
 		}
 	        printf("\n");
         }
+	printf("\n");
     return 0;
     }
-
-
-int clasificar(int *red, int dim)
+//*************************************************************
+int imprimirVector(int *vect, int dim)
+    {
+        int k;
+        for (k=0; k<dim*dim; k++)
+	{printf("%d", *(vect+k));
+        }
+	 printf("\n");
+	
+    return 0;
+    }
+//*************************************************************
+int clasificar(int *red, int dim,int *historial,int etiqueta)
 {
-	int *historial;
-	historial=(int *)malloc(dim*sizeof(int));
+	
 	int j,i;	
-	int etiqueta=2;
 	int s1;
 	int s2;
-	for(j=0;j<dim*dim;j++)//Armar el historial
-	{*(historial+j)=j;	
-	}
+
 	
 	
 	
@@ -87,90 +121,68 @@ int clasificar(int *red, int dim)
 	for(i=1; i<dim;i++)//La primer fila
 	{	
 		if(*(red+i)>0)// Si tiene un 1
-		{
-			if(*(red+(i-1))>0) // Si la de atrás tiene un 1
-			{		
-			actualizar(red+i,historial,*(red+(i-1)),etiqueta);
-			}else{ // Si tiene que poner una etiqueta nueva
-
-			*(red+i)=etiqueta;
-			etiqueta++;
-			}
-		}
+			etiqueta=actualizar(red+i,historial,*(red+(i-1)),etiqueta);
+				
 	}
 	
 	for(j=1;j<dim;j++) //el resto de las filas
 	{
 
 		if(*(red+j*dim)>0) // La primer columna de cada fila, si tiene un 1
-		{
-
-			if(*(red+(j-1)*dim)>0) // Si la de arriba tiene un 1
-			{ 
-			actualizar(red+j*dim,historial,*(red+(j-1)*dim),etiqueta);
-			}else{//Si tiene que agregar una nueva etiqueta
-
-			*(red+j*dim)=etiqueta;
-			etiqueta++;
-			}
-		}
+			etiqueta=actualizar(red+j*dim,historial,*(red+(j-1)*dim),etiqueta);
+		
 		for(i=1;i<dim;i++) // el resto de las columnas
 		{
 
 				if(*(red+j*dim+i)>0) // si tiene un 1
-
 				{
 					
-					s1=*(red+j*dim+i-1); // la de arriba
-					s2=*(red+(j-1)*dim+i);	// la de la izquierda
+					s1=*(red+j*dim+i-1); // la de la izquierda
+					s2=*(red+(j-1)*dim+i);	// la de arriba
 
-					if(s1+s2==0)
-					{
-					*(red+j*dim+i)=etiqueta;
-					etiqueta++;	
-					}else{
 						if(s1*s2>0) // si las dos son positivas
 						{
-
 						etiqueta_falsa((red+j*dim+i),historial,s1,s2,etiqueta);
-
 						}
 						else
 						{
 							if(s1>0)
-							{actualizar((red+j*dim+i),historial,s1,etiqueta);
+							{etiqueta=actualizar((red+j*dim+i),historial,s1,etiqueta);
 							}
 							else
-							{actualizar((red+j*dim+i),historial,s2,etiqueta);
+							{etiqueta=actualizar((red+j*dim+i),historial,s2,etiqueta);
 							}
 						}
-					}
+					
 				}
 		}
 		
 	}
-	printf("\n Historial: ");
-	for (i=0;i<dim*dim;i++)
-	{
-		printf("%d",*(historial+i));
-	}	
-	printf("\n");
+
 	return 0;
 }
 
+//*************************************************************
 
-
-int actualizar(int *local,int *historial,int s,int frag)
+int actualizar(int *local,int *historial,int s,int etiqueta)
 {
+
+	if (s>0)
+	{
 	while(*historial+s<0)
 		{
-			s=-*historial+s;
+			s=-(*historial+s);
 		}
 	*local=s;
-	return 0;
+	}else{
+	*local=etiqueta;
+	etiqueta++;
+		
+	}
+	return etiqueta;
 }
-
-int etiqueta_falsa(int *local, int *historial, int s1, int s2, int frac)
+//*************************************************************
+int etiqueta_falsa(int *local, int *historial, int s1, int s2, int etiqueta)
 {
 	int mini,maxi;
 	while(*historial+s1<0)
@@ -193,7 +205,60 @@ int etiqueta_falsa(int *local, int *historial, int s1, int s2, int frac)
 	*(historial+maxi)=-mini;
 	*(historial+mini)=mini;
 	return 0;
+}
+//*************************************************************
+int arreglar_etiquetas(	int *red,int *historial, int dim)
+{
+int i;
+int s;
+for (i=0; i<dim*dim; i++)
+{	s=*(historial+*(red+i));
+	while(s<0)
+	{
+	s= *(historial+(-1)* *(red+s));	
+	}
+	*(red+i)=s;
 }	
+return 0;
+}
+
+//*************************************************************
+int percola(int *red, int dim)
+{
+int i;
+int j=0;
+int b=0;
+int *vect1;
+vect1=(int *)malloc(dim*dim*sizeof(int)); 
+int *vect2;
+vect2=(int *)malloc(dim*dim*sizeof(int)); 
+
+
+
+for (i=0; i<dim; i++)
+{
+
+	if (*(red+i)>0)
+	{
+	*(vect1+*(red+i))=1;
+	}if(*(red+(dim*(dim-1))+i))
+	{
+	*(vect2+*(red+(dim*(dim-1))+i))=1;
+	}
+}
+
+while (*(vect1+j) * *(vect2+j)==0 && j<dim*dim)
+{
+j++;
+}
+
+if (j<dim*dim)
+b=1;
+
+return b;
+}
+//*************************************************************
+
 // Creo que anda bien. Habría que revisar un poco más que pasa con más casos. 
 // Falta: Reetiquetar todo, ver si percola o no
 
