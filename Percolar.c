e366d1a4a77177bcf56e0b0919e6bafc17a93fe1
lupipedraza <42683132+lupipedraza@users.noreply.github.com>
2019-04-03 15:07:10 -0300

    Add files via upload

--- Percolar.c
+++ Percolar.c
@@ -1,5 +1,4 @@
 
-
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -7,17 +6,25 @@
 
 int poblar(int *red,float p,int dim);
 int imprimir(int *red, int dim);
+int clasificar(int *red, int dim);
+int actualizar(int *local,int *historial,int s,int frag);
+int etiqueta_falsa(int *local, int *historial, int s1, int s2, int frac);
+
 int main()
 
 {
-	int dim = 3;
+	int dim = 5;
     	float p = 0.5;
 	int *red;
 	red=(int *)malloc(dim*dim*sizeof(int));  
 	poblar(red, p,dim);
 	imprimir(red,dim);
-	//	clasificar(*red)
-	free(red);
+	clasificar(red,dim);
+	printf("\n");
+	imprimir(red,dim);
+	free(red);	
+	printf("\n");
+	
 	return 0;
 }
 
@@ -57,58 +64,136 @@ int imprimir(int *red, int dim)
     return 0;
     }
 
-/*
 
 int clasificar(int *red, int dim)
 {
-	if (red)
-	{
-	*red=2
+	int *historial;
+	historial=(int *)malloc(dim*sizeof(int));
+	int j,i;	
+	int etiqueta=2;
+	int s1;
+	int s2;
+	for(j=0;j<dim*dim;j++)//Armar el historial
+	{*(historial+j)=j;	
 	}
-	for(i=1, i<dim,i++)
+	
+	
+	
+	if (*red)// la primer celda
+	{
+	*red=etiqueta;
+	etiqueta++;
+	}
+	for(i=1; i<dim;i++)//La primer fila
 	{	
-		if(red+i)
+		if(*(red+i)>0)// Si tiene un 1
 		{
-			if(red+(i-1))		
-			actualizar(red+i,historial,s,frag);
-			
+			if(*(red+(i-1))>0) // Si la de atrás tiene un 1
+			{		
+			actualizar(red+i,historial,*(red+(i-1)),etiqueta);
+			}else{ // Si tiene que poner una etiqueta nueva
+
+			*(red+i)=etiqueta;
+			etiqueta++;
+			}
 		}
 	}
-	for(i=1,i<dim,i++)
+	
+	for(j=1;j<dim;j++) //el resto de las filas
 	{
-		acutalizar(red+i*dim,historial,s,frag);
-		for(j=1,j<dim,j++)
+
+		if(*(red+j*dim)>0) // La primer columna de cada fila, si tiene un 1
 		{
-			if(s1*s2)
-			{
-				etiqueta_falsa()
-			}
-			else
-			{
-				if(s1)
-				{actializar(,s1)}
-				else
-				{actializar(,s2)}
+
+			if(*(red+(j-1)*dim)>0) // Si la de arriba tiene un 1
+			{ 
+			actualizar(red+j*dim,historial,*(red+(j-1)*dim),etiqueta);
+			}else{//Si tiene que agregar una nueva etiqueta
+
+			*(red+j*dim)=etiqueta;
+			etiqueta++;
 			}
 		}
-	}
+		for(i=1;i<dim;i++) // el resto de las columnas
+		{
+
+				if(*(red+j*dim+i)>0) // si tiene un 1
+
+				{
+					
+					s1=*(red+j*dim+i-1); // la de arriba
+					s2=*(red+(j-1)*dim+i);	// la de la izquierda
+
+					if(s1+s2==0)
+					{
+					*(red+j*dim+i)=etiqueta;
+					etiqueta++;	
+					}else{
+						if(s1*s2>0) // si las dos son positivas
+						{
+
+						etiqueta_falsa((red+j*dim+i),historial,s1,s2,etiqueta);
+
+						}
+						else
+						{
+							if(s1>0)
+							{actualizar((red+j*dim+i),historial,s1,etiqueta);
+							}
+							else
+							{actualizar((red+j*dim+i),historial,s2,etiqueta);
+							}
+						}
+					}
+				}
+		}
 		
+	}
+	printf("\n Historial: ");
+	for (i=0;i<dim*dim;i++)
+	{
+		printf("%d",*(historial+i));
+	}	
+	printf("\n");
+	return 0;
 }
+
+
 
 int actualizar(int *local,int *historial,int s,int frag)
 {
-
 	while(*historial+s<0)
 		{
 			s=-*historial+s;
 		}
-	*local=s
+	*local=s;
+	return 0;
 }
 
-int etiqueta_falsa(*int local, int ĥistorial, int s1, int s2, int frac)
+int etiqueta_falsa(int *local, int *historial, int s1, int s2, int frac)
 {
-
-}
-
-*/
+	int mini,maxi;
+	while(*historial+s1<0)
+	{
+		s1=-(*(historial+s1));
+	}
+	while(*historial+s2<0)
+	{
+		s2=-(*(historial+s2));
+	}
+	if(s1<s2)
+	{
+	mini=s1;
+	maxi=s2;
+	}else{
+	mini=s2;
+	maxi=s1;
+	}
+	*local=mini;
+	*(historial+maxi)=-mini;
+	*(historial+mini)=mini;
+	return 0;
+}	
+// Creo que anda bien. Habría que revisar un poco más que pasa con más casos. 
+// Falta: Reetiquetar todo, ver si percola o no
 
